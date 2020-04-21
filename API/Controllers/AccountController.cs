4319f3c7075c9e1dd39f3d57c99e7e9f201ec2a4
djunh1 <djunh1@gmail.com>
2020-04-21 17:13:11 -0400

    Add validation for registration, address, and basket

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -99,6 +99,12 @@ namespace API.Controllers
     [HttpPost("register")]
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
+
+        if (CheckEmailExistsAsynch(registerDto.Email).Result.Value){
+            return new BadRequestObjectResult(new ApiValidationErrorResponse{
+                Errors = new []{"This email address is in use."}
+            });
+        }
         var user = new AppUser
         {
             DisplayName = registerDto.DisplayName,
--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -8,8 +8,25 @@ namespace API.Extensions
     {
         public static IServiceCollection AppSwaggerDocumentation(this IServiceCollection services){
             services.AddSwaggerGen(c => {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "HockeyNew API", Version = "v1"});
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "HockeyNet API", Version = "v1"});
+                
+                var securitySchema = new OpenApiSecurityScheme {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
 
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement {{ securitySchema, new[] {
+                    "Bearer"
+                }}};
+                c.AddSecurityRequirement(securityRequirement);
             });
 
             return services;
